e48bebc6532ed171459ec91542df037a2f9cdb54
Milus1211 <milagrospayano8094577242@gmail.com>
2025-08-02 11:08:28 -0400

    Actualizacion en Servicios

--- Proyecto Final (Sistema de Spa)/FormActualizarCita.Designer.cs
+++ Proyecto Final (Sistema de Spa)/FormActualizarCita.Designer.cs
@@ -36,34 +36,44 @@
             this.btnGuardar = new Guna.UI2.WinForms.Guna2Button();
             this.dtpFecha = new System.Windows.Forms.DateTimePicker();
             this.dtpHora = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtTerapeuta
             // 
-            this.txtTerapeuta.Location = new System.Drawing.Point(52, 178);
+            this.txtTerapeuta.Location = new System.Drawing.Point(115, 179);
+            this.txtTerapeuta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtTerapeuta.Name = "txtTerapeuta";
-            this.txtTerapeuta.Size = new System.Drawing.Size(100, 22);
+            this.txtTerapeuta.Size = new System.Drawing.Size(206, 26);
             this.txtTerapeuta.TabIndex = 0;
             // 
             // txtServicio
             // 
-            this.txtServicio.Location = new System.Drawing.Point(52, 125);
+            this.txtServicio.Location = new System.Drawing.Point(109, 124);
+            this.txtServicio.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtServicio.Name = "txtServicio";
-            this.txtServicio.Size = new System.Drawing.Size(100, 22);
+            this.txtServicio.Size = new System.Drawing.Size(212, 26);
             this.txtServicio.TabIndex = 1;
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(52, 76);
+            this.txtNombre.Location = new System.Drawing.Point(109, 81);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.Size = new System.Drawing.Size(212, 26);
             this.txtNombre.TabIndex = 2;
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(52, 38);
+            this.txtID.Location = new System.Drawing.Point(109, 47);
+            this.txtID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(100, 22);
+            this.txtID.Size = new System.Drawing.Size(112, 26);
             this.txtID.TabIndex = 3;
             // 
             // btnBuscar
@@ -74,9 +84,10 @@
             this.btnBuscar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btnBuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnBuscar.ForeColor = System.Drawing.Color.White;
-            this.btnBuscar.Location = new System.Drawing.Point(33, 261);
+            this.btnBuscar.Location = new System.Drawing.Point(37, 326);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(180, 45);
+            this.btnBuscar.Size = new System.Drawing.Size(202, 56);
             this.btnBuscar.TabIndex = 5;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
@@ -89,9 +100,10 @@
             this.btnGuardar.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
             this.btnGuardar.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.btnGuardar.ForeColor = System.Drawing.Color.White;
-            this.btnGuardar.Location = new System.Drawing.Point(264, 261);
+            this.btnGuardar.Location = new System.Drawing.Point(297, 326);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(180, 45);
+            this.btnGuardar.Size = new System.Drawing.Size(202, 56);
             this.btnGuardar.TabIndex = 6;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
@@ -99,25 +111,87 @@
             // dtpFecha
             // 
             this.dtpFecha.CustomFormat = "\"dd-MM-yyyy\";";
-            this.dtpFecha.Location = new System.Drawing.Point(445, 129);
+            this.dtpFecha.Location = new System.Drawing.Point(522, 81);
+            this.dtpFecha.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtpFecha.Name = "dtpFecha";
-            this.dtpFecha.Size = new System.Drawing.Size(200, 22);
+            this.dtpFecha.Size = new System.Drawing.Size(224, 26);
             this.dtpFecha.TabIndex = 7;
             // 
             // dtpHora
             // 
             this.dtpHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpHora.Location = new System.Drawing.Point(445, 176);
+            this.dtpHora.Location = new System.Drawing.Point(522, 130);
+            this.dtpHora.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dtpHora.Name = "dtpHora";
             this.dtpHora.ShowUpDown = true;
-            this.dtpHora.Size = new System.Drawing.Size(200, 22);
+            this.dtpHora.Size = new System.Drawing.Size(224, 26);
             this.dtpHora.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Servicio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Terapeuta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(435, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Fecha";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(435, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Hora";
             // 
             // FormActualizarCita
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dtpHora);
             this.Controls.Add(this.dtpFecha);
             this.Controls.Add(this.btnGuardar);
@@ -126,6 +200,7 @@
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.txtServicio);
             this.Controls.Add(this.txtTerapeuta);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "FormActualizarCita";
             this.Text = "FormActualizarCita";
             this.Load += new System.EventHandler(this.FormActualizarCita_Load);
@@ -144,5 +219,11 @@
         private Guna.UI2.WinForms.Guna2Button btnGuardar;
         private System.Windows.Forms.DateTimePicker dtpFecha;
         private System.Windows.Forms.DateTimePicker dtpHora;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
